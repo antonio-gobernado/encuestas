2c7ef543de7985d10d360489312123852cc0dbcc
antonio.gobernado <gober.bar@gmail.com>
2021-06-28 02:11:13 +0200

    second

--- poll/settings.py
+++ poll/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^9396k93*2h2x_b$2n4u*ov&#x%n=_@p56=(tkga&*lan=!6j$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
